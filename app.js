27fdfef91ba0f49bd2f818698c33356458b4cde2
LPT <lptalty@gmail.com>
2021-03-10 11:11:08 -0500

    I spent a lot of time going back and forth with how to make the grid dynamic. The actual JS is not complicated, but being able to manipulate the DOM and update CSS based on the input of the user was my main challenge. I had to research get element by ID (which ended up not being appropriate for this situation) and then learn about query selectors. Now whatever number the user inputs after being prompted, populated the container.

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
 const body = document.body
-//const boxDiv = document.createElement("div")
-//boxDiv.setAttribute("id", "boxDiv")
-//boxDiv.innerText = 'working'
 
 const container = document.createElement("div")
 container.setAttribute("id", "container")
 
+
+//let user input = input number
+let userInput = prompt('What size grid do you want?')
+//x and y are less than user input
+
+if(userInput >= 4 && userInput <=25){
+    //user input has to be in between 2 and 25(? not sure how high the upper limit should be)
 //whatever x and y are is the number that needs to be repeated within css
 //this will eventually be whatever amount the user decides to input
-//I could this with a button or have them actually write out a number
-//i think that having three buttons for different grids may be optimal because they may not put in perfect squares
-for(x = 0; x < 16; x++){
-    for(let y = 0; y < 16; y++){
+for(x = 0; x < userInput; x++){
+    for(let y = 0; y < userInput; y++){
         const boxDiv = document.createElement("div")
         boxDiv.setAttribute("id", "boxDiv")
-        boxDiv.innerText = 'w'
+        //boxDiv.innerText = 'w'
         container.append(boxDiv)
     }
 }
+}
 
-
-
-//container.append(boxDiv)
 body.append(container)
+
+//const changeContainer = document.querySelector("#container").style.width = "300px"
+//changeContainer.style.width = 200px
+//console.log(changeContainer)
+
+document.querySelector("#container").style.gridTemplateColumns = `repeat(${userInput}, 1fr)`
+
+
+
+// let gridSize = document.querySelector('container')
+// gridSize.style.gridTemplateColumns = repeat(userInput, 1fr);
+
