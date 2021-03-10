9765c26c8c5532cbb9a5ea53d9db7add733fc919
LPT <lptalty@gmail.com>
2021-03-10 11:34:10 -0500

    Cleaned up my global execution context. I made a function that populates the grid based on the amount the user specifies. I also reprompt the user in case the value is not within the correct range. Finally, I tried to clean up my psuedo code to explain the function more concisely.

--- app.js
+++ app.js
@@ -1,37 +1,44 @@
 const body = document.body
-
 const container = document.createElement("div")
 container.setAttribute("id", "container")
 
+const userInput = prompt('What size grid do you want?')
 
-//let user input = input number
-let userInput = prompt('What size grid do you want?')
-//x and y are less than user input
+function gridSize(num){
 
-if(userInput >= 4 && userInput <=25){
-    //user input has to be in between 2 and 25(? not sure how high the upper limit should be)
-//whatever x and y are is the number that needs to be repeated within css
-//this will eventually be whatever amount the user decides to input
-for(x = 0; x < userInput; x++){
-    for(let y = 0; y < userInput; y++){
+//if the original value given by the user is within the correct range
+if(num >= 2 && num <=64){
+//create square grid based on the argument assigned to the parameter num
+for(x = 0; x < num; x++){
+    for(let y = 0; y < num; y++){
         const boxDiv = document.createElement("div")
         boxDiv.setAttribute("id", "boxDiv")
-        //boxDiv.innerText = 'w'
         container.append(boxDiv)
     }
+    }   
+//add container id to body so that it can be manipulated using the DOM
+//style the container grid, ensuring the number of rows and columns is equal to the argument value
+    body.append(container)
+    document.querySelector("#container").style.gridTemplateColumns = `repeat(${num}, 1fr)`
+
+
+//else reprompt the user for a new number that is inbetween the correct range
+} else {
+
+    //reprompt user for new argument value that fits within the range
+    let updatedInput = prompt('Please choose a number between 2 and 64')
+
+    //invoke the gridSize function again with the correct argument value
+    gridSize(updatedInput)
 }
+
+
+
+
+
 }
 
-body.append(container)
-
-//const changeContainer = document.querySelector("#container").style.width = "300px"
-//changeContainer.style.width = 200px
-//console.log(changeContainer)
-
-document.querySelector("#container").style.gridTemplateColumns = `repeat(${userInput}, 1fr)`
+gridSize(userInput)
 
 
-
-// let gridSize = document.querySelector('container')
-// gridSize.style.gridTemplateColumns = repeat(userInput, 1fr);
 
