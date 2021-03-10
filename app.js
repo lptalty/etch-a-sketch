2d71b4ea66a5363355d7276e1781a14431c4af40
LPT <lptalty@gmail.com>
2021-03-10 09:15:17 -0500

    I managed to make the box appear how I like, but I still have to manually adjust CSS to populate the container div correctly. I want the user to be able to decide the size of the box, but considering I want the number to be a perfect square, I may try and implement buttons that the user can press, rather than prompting them to type out a response.

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const body = document.body
-const boxDiv = document.createElement("div")
-boxDiv.setAttribute("id", "boxDiv")
-boxDiv.innerText = 'working'
+//const boxDiv = document.createElement("div")
+//boxDiv.setAttribute("id", "boxDiv")
+//boxDiv.innerText = 'working'
 
 const container = document.createElement("div")
 container.setAttribute("id", "container")
 
+//whatever x and y are is the number that needs to be repeated within css
+//this will eventually be whatever amount the user decides to input
+//I could this with a button or have them actually write out a number
+//i think that having three buttons for different grids may be optimal because they may not put in perfect squares
+for(x = 0; x < 16; x++){
+    for(let y = 0; y < 16; y++){
+        const boxDiv = document.createElement("div")
+        boxDiv.setAttribute("id", "boxDiv")
+        boxDiv.innerText = 'w'
+        container.append(boxDiv)
+    }
+}
 
-container.append(boxDiv)
+
+
+//container.append(boxDiv)
 body.append(container)
